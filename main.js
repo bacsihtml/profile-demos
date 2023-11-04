939bbcc5bfb1b3fc24a18b523cc7a8c8f280d4ff
bacsihtml <caywa312@gmail.com>
2023-11-04 14:13:21 +0700

    source code new

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Youtuber", "Web Devaloper"],
+    typeSpeed: 100, /*thời gian hiện ra từng chữ cái là 0.1s */
+    backSpeed: 100,  /*thời gian xóa từng chữ cái là 0.1s */
+    backDelay: 1000, /*thời gian chờ trước khi xóa các chữ cái là 1s */
+    loop:true
+}); /*chú ý cú pháp này ko đc sai */
